423a3a07af7f177468465c72d2210856bb297d38
ednyc <88912058+ednyc@users.noreply.github.com>
2025-08-26 23:05:02 +0000

    out

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
   distDir: 'docs',
-  
+  basePath: isProd ? '/0826457.github.io-3n' : '',
+  assetPrefix: isProd ? '/0826457.github.io-3n/' : '',
   eslint: {
     ignoreDuringBuilds: true,
   },
